210f4e86f645d8c6e52489cb6df4b3f456440b00
hesphoros <hesphoros@gmail.com>
2026-06-02 01:55:02 +0800

    initial commit

---
+++ include/lazy_importer_plus.hpp
@@ -0,0 +1,45 @@
+/*****************************************************************************
+*  LazyImporterPlus                                                        
+*  Copyright (C) 2026 hesphoros <hesphoros@gmail.com>
+*                                                                           
+*  This file is part of LazyImporterPlus.                                  
+*                                                                           
+*  This program is free software; you can redistribute it and/or modify    
+*  it under the terms of the GNU General Public License version 3 as       
+*  published by the Free Software Foundation.                              
+*                                                                           
+*  You should have received a copy of the GNU General Public License       
+*  along with this program. If not, see <http://www.gnu.org/licenses/>.    
+*                                                                           
+*  @file     lazy_impoter_plus.hpp                                                
+*  @brief    Lazy Importer cross platform library header file.
+*  @details  This header file defines the interface for the Lazy Importer P
+*  lus library, which provides functionality for lazy importing of functions 
+*  and modules in a cross-platform manner. The library is designed to simplify
+*  the process of dynamically loading and using functions from shared libraries 
+* (DLLs on Windows, .so files on Linux) at runtime, without the need for
+*  explicit linking at compile time. It abstracts away platform-specific
+*  details and provides a unified API for developers to work with.
+*                                                                           
+*  @author   hesphoros                                                
+*  @email    hesphoros@gmail.com                                   
+*  @version  1.0.0.1                                                       
+*  @date     2026/06/02             
+*  @license  GNU General Public License (GPL)                              
+*---------------------------------------------------------------------------*
+*  Remark         :                                           
+*---------------------------------------------------------------------------*
+*  Change History :                                                        
+*  <Date>     | <Version> | <Author>       | <Description>                 
+*  2026/06/02 | 1.0.0.1   | hesphoros      | Create file           
+*****************************************************************************/
+
+#ifndef LAZY_IMPOTER_PLUS_HPP
+#define LAZY_IMPOTER_PLUS_HPP
+
+#include <utility>
+#include <intrin.h>
+
+
+
+#endif //LAZY_IMPOTER_PLUS_HPP
